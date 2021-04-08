b943b94b66ad1f83e513df5787a86dfb3d2262c2
Brian Reich <reichman213@gmail.com>
2021-04-08 09:01:51 -0500

    add custom tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,46 @@
+const colors = require('tailwindcss/colors');
+
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // teal: {
+        //   50: "#F0FDFA",
+        //   100: "#CCFBF1",
+        //   200: "#99F6E4",
+        //   300: "#5EEAD4",
+        //   400: "#2DD4BF",
+        //   500: "#14B8A6",
+        //   600: "#0D9488",
+        //   700: "#0F766E",
+        //   800: "#115E59",
+        //   900: "#134E4A",
+        // }, 
+        // cyan: {
+        //   50: "#ECFEFF",
+        //   100: "#CFFAFE",
+        //   200: "#A5F3FC",
+        //   300: "#67E8F9",
+        //   400: "#22D3EE",
+        //   500: "#06B6D4",
+        //   600: "#0891B2",
+        //   700: "#0E7490",
+        //   800: "#155900",
+        //   900: "#164E63",
+        // },
+        teal: colors.teal,
+        cyan: colors.cyan
+      },
+      fontFamily: {
+        raleway: ['Raleway', 'Helvetica', 'Arial', 'sans-sserif']
+      },
+      borderWidth: {
+        '1.5': '1.5px'
+      }
+    },
   },
   variants: {
     extend: {},

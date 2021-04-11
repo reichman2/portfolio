e280d081feb469bc64bff1feb062359188cea738
Brian Reich <reichman213@gmail.com>
2021-04-11 11:36:20 -0500

    add mdx support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/
+})
 const isProd = (process.env.NODE_ENV || 'production') === 'production';
 
 module.exports = {
     // distDir: 'build',
+    withMDX: ({
+        pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+        target: 'serverless'
+    })
 };

37e804c2edcc157ed3d8cd2511482a5d286625fd
Brian Reich <reichman213@gmail.com>
2022-05-09 14:49:55 -0600

    fix distDir -- shouldn't be defined

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withMDX = require('@next/mdx')({
 const isProd = (process.env.NODE_ENV || 'production') === 'production';
 
 module.exports = {
-    distDir: 'build',
+    // distDir: 'build', // leave undefined in prod
     withMDX: ({
         pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
         target: 'serverless',

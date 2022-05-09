ff7543d5fbf3131b23fae1b2ab9e841339b11167
Brian Reich <reichman213@gmail.com>
2022-05-09 14:07:47 -0600

    define dist directory

--- next.config.js
+++ next.config.js
@@ -4,9 +4,10 @@ const withMDX = require('@next/mdx')({
 const isProd = (process.env.NODE_ENV || 'production') === 'production';
 
 module.exports = {
-    // distDir: 'build',
+    distDir: 'build',
     withMDX: ({
         pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-        target: 'serverless'
+        target: 'serverless',
+        
     })
 };

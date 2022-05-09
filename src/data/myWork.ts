c8a3f7ec3db53337a05a81b6b9c319fd8b596cbc
Brian Reich <reichman213@gmail.com>
2022-05-09 14:13:57 -0600

    remove incomplete/nei projects

--- src/data/myWork.ts
+++ src/data/myWork.ts
@@ -1,28 +1,22 @@
 type Project = {
     name: string;
     description: string;
-    image?: string;
-    link?: string;
+    image?: ImgData;
+    link: string;
+}
+
+type ImgData = {
+    src: string;
+    alt?: string;
+    width: number;
+    height: number;
 }
 
 const projects: Array<Project> = [
     {
-        name: "Spotify DJ",
-        description: "",
-        image: "",
-        link: ""
-    },
-    {
-        name: "Deerfield Kiva Club",
-        description: "",
-        image: "",
-        link: ""
-    },
-    {
         name: "Training Plan API",
         description: "An algorithm to parse a PDF file into a JavaScript object and an API to retrieve that information and put it into an iPhone widget.",
-        image: "",
-        link: ""
+        link: "/blog/TrainingPlanApi"
     }
 ]
 
